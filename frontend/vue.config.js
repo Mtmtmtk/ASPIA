b2ad51c04bc922396d520f74585175f31c967e47
ssmsaito <saito@iflab.co.jp>
2021-04-08 11:39:11 +0900

    fix commit error

---
+++ frontend/vue.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production' ? '/' : '/vue-dev/',
+    devServer: {
+        port: 80,
+        public: process.env.DOMAIN_NAME,
+    },
+    configureWebpack: {
+        resolve: { symlinks: false },
+        devtool: 'source-map',
+        //watch: true,
+        //watchOptions: {
+        //    aggregateTimeout: 1000,
+        //    poll: 5000,
+        //    ignored: /node_modules/
+        //},
+        devServer: {
+            watchOptions: {
+                poll: true
+            }
+        },
+        //optimization: {
+        //    splitChunks: {
+        //        minSize: 10000,
+        //        maxSize: 250000,
+        //    }
+        //}
+    }
+};
