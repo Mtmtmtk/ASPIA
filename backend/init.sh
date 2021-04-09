94945f386ada799bf118880ff853242b387c22c8
ssmsaito <saito@iflab.co.jp>
2021-04-09 16:24:38 +0900

    frontend and backend build production by restarting the container

--- backend/init.sh
+++ backend/init.sh
@@ -3,4 +3,10 @@
 rm -rf .pid/
 envsubst '$$REDIS_ADDRESS' < ${BACKEND_HOME}/config_server.ini.template > ${BACKEND_HOME}/config_server.ini
 python -m ducts server start &
-watchmedo shell-command -W -R -p '*.py' -c 'echo ${watch_src_path}; python -m ducts server stop && python -m ducts server start' ./
+
+if [ "$1" = "dev" ]; then
+    watchmedo shell-command -W -R -p '*.py' -c 'echo ${watch_src_path}; python -m ducts server stop && python -m ducts server start' ./
+else
+    /bin/bash
+fi
+
