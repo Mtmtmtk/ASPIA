6781755a944049995505d4487f65bb5e0723aa2d
ssmsaito <saito@iflab.co.jp>
2021-04-09 21:33:59 +0900

    dev redis

--- backend/init.sh
+++ backend/init.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 rm -rf .pid/
-envsubst '$$REDIS_ADDRESS' < ${BACKEND_HOME}/config_server.ini.template > ${BACKEND_HOME}/config_server.ini
-python -m ducts server start &
+envsubst '$$REDIS_ADDRESS' < $HOME/config_server.ini.template > $HOME/config/config_server.ini
+python -m ducts server start -c ./config/config_server.ini &
 
 if [ "$1" = "dev" ]; then
-    watchmedo shell-command -W -R -p '*.py' -c 'echo ${watch_src_path}; python -m ducts server stop && python -m ducts server start' ./
+    watchmedo shell-command -W -R -p '*.py' -c 'echo ${watch_src_path}; python -m ducts server stop && python -m ducts server start -c ./config/config_server.ini' ./
 else
     /bin/bash
 fi
