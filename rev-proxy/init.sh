64a8d9035e4c1eb0ad9568dac708676057048c8b
Ubuntu <ubuntu@ip-172-31-37-14.us-east-2.compute.internal>
2021-04-12 06:21:26 +0000

    enable_ssl

--- rev-proxy/init.sh
+++ rev-proxy/init.sh
@@ -1,14 +1,14 @@
 #!/bin/ash
 
 if [ "$1" = "prodonly" ]; then
-    RUN envsubst '$$PORT $$DOMAIN_NAME $$BACKEND_ADDRESS' < /etc/nginx/nginx.conf.template.prodonly > /etc/nginx/nginx.conf
+    envsubst '$$PORT $$DOMAIN_NAME $$BACKEND_ADDRESS' < /etc/nginx/nginx.conf.template.prodonly > /etc/nginx/nginx.conf
 else
-    RUN envsubst '$$PORT $$DEV_PORT $$DOMAIN_NAME $$DEV_DOMAIN_NAME $$FRONTEND_DEV_ADDRESS $$BACKEND_ADDRESS $$BACKEND_DEV_ADDRESS' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+    envsubst '$$PORT $$DEV_PORT $$DOMAIN_NAME $$DEV_DOMAIN_NAME $$FRONTEND_DEV_ADDRESS $$BACKEND_ADDRESS $$BACKEND_DEV_ADDRESS' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 fi
 
 nginx
 
-if [ $ENABLE_SSL = 1 ] ; then
+if [ $ENABLE_SSL -eq 1 ] ; then
     certbot --nginx -d ${DOMAIN_NAME} -m ${EMAIL} --agree-tos -n
     certbot renew
     nginx -s reload
