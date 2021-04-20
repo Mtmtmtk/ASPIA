eb73cfd68894ae8bd05e39293927767a1fecd622
ssmsaito <saito@iflab.co.jp>
2021-04-20 18:49:16 +0900

    ssl for dev, nginx.conf bug fix for index.html routing, http port for prod server

--- rev-proxy/init.sh
+++ rev-proxy/init.sh
@@ -3,15 +3,21 @@
 chmod +rx /var/www/html/dist
 
 if [ "$1" = "prodonly" ]; then
-    envsubst '$$PORT $$DOMAIN_NAME $$BACKEND_ADDRESS' < /etc/nginx/nginx.conf.template.prodonly > /etc/nginx/nginx.conf
+    envsubst '$$HTTP_PORT $$DOMAIN_NAME $$BACKEND_ADDRESS' < /etc/nginx/nginx.conf.template.prodonly > /etc/nginx/nginx.conf
 else
-    envsubst '$$PORT $$DEV_PORT $$DOMAIN_NAME $$DEV_DOMAIN_NAME $$FRONTEND_DEV_ADDRESS $$BACKEND_ADDRESS $$BACKEND_DEV_ADDRESS' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+    envsubst '$$HTTP_PORT $$DEV_HTTP_PORT $$DOMAIN_NAME $$DEV_DOMAIN_NAME $$DEV_FRONTEND_ADDRESS $$BACKEND_ADDRESS $$DEV_BACKEND_ADDRESS' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 fi
 
 nginx
 
 if [ $ENABLE_SSL -eq 1 ] ; then
     certbot --nginx -d ${DOMAIN_NAME} -m ${EMAIL} --agree-tos -n
+    certbot renew
+    nginx -s reload
+fi
+
+if [ $DEV_ENABLE_SSL -eq 1 ] ; then
+    certbot --nginx -d ${DEV_DOMAIN_NAME} -m ${DEV_EMAIL} --agree-tos -n
     certbot renew
     nginx -s reload
 fi
