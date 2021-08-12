49b1f58e9c5af8cedbeda8682fb8a54be7745820
ssmsaito <>
2021-08-12 17:01:06 +0900

    removed Dokerfile-dev for backend

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,7 +4,7 @@ rm -rf .pid/
 envsubst '$$REDIS_ADDRESS' < $HOME/config/config_server.ini.template > $HOME/config/config_server.ini
 python -m ducts server start -c ./config/config_server.ini &
 
-if [ "$1" = "dev" ]; then
+if [ "$SERVICE_NAME" = "backend-dev" ]; then
     watchmedo shell-command -W -R -p '*.py' -c 'echo ${watch_src_path}; python -m ducts server stop && python -m ducts server start -c ./config/config_server.ini' ./
 else
     /bin/bash
